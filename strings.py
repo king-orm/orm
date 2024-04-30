e334edb5938792417169b9f9c84ececdae90782e
Anatoly Frolov <44891639+anafro@users.noreply.github.com>
2024-04-30 23:09:46 +1000

    Add types to `strings.py`
    Co-Authored-By: Alexander Bavykin <bavykin.03@mail.ru>

--- strings.py
+++ strings.py
@@ -1,4 +1,4 @@
-def pluralize(noun):
+def pluralize(noun: str):
     if noun.endswith('s') or noun.endswith('x') or noun.endswith('z') or noun.endswith('sh') or noun.endswith('ch'):
         return noun + 'es'
     elif noun.endswith('y') and noun[-2] not in 'aeiou':
